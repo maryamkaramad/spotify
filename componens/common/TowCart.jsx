781997f798148857e0f834b41ec6b8a9fa8baa77
maryamkaramad <maryamkaramad1373@gmail.com>
2022-11-29 13:58:43 +0330

    add cursor in tow cart

--- componens/common/TowCart.jsx
+++ componens/common/TowCart.jsx
@@ -4,10 +4,10 @@ import Image from 'next/image';
 
 const TowCart = ({ img, title1, title2 }) => {
     return (
-        <Grid item xs={1.5} boxShadow={1} sx={{ background: "linear-gradient(180deg, #3C3F41 0%, #000000 100%)" }} borderRadius={"4px"} >
+        <Grid item xs={1.5} boxShadow={1} sx={{ background: "linear-gradient(180deg, #3C3F41 0%, #000000 100%)" }} borderRadius={"4px"}  >
             <Grid xs={12} item container display={"flex"} flexDirection={"column"} alignItems={"center"} p={1} justifyContent={"center"} alignContent={"center"} gap={4}>
                 <Grid xs={12} item display={"flex"} justifyContent={"center"} alignItems={"center"}>
-                    <Image src={img} alt={title1} />
+                    <Image src={img} alt={title1} style={{ cursor: "pointer" }} />
                 </Grid>
                 <Grid xs={12} item display={"flex"} justifyContent={"flex-start"} flexDirection={"column"}>
                     <Typography variant={'subtitle3'}>
